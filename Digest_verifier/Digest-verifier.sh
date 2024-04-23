1f3bb0368d7a2568f6e45c6dbb4902a299f088b0
Mohammadi Iram <89964724+MohammadiIram@users.noreply.github.com>
2024-04-23 13:30:45 +0530

    Enhancements to digest verifier script (#42)
    * Enhancements to digest verifier script
    ---------
    
    Co-authored-by: Mohammadi Iram <miram@redhat.com>

--- Digest_verifier/Digest-verifier.sh
+++ Digest_verifier/Digest-verifier.sh
@@ -1,94 +1,97 @@
 #!/bin/bash
 
-# Path to the file containing the repository URL
-REPO_URL_FILE="./Digest_verifier/repo_url.txt"
+# File containing the repository URL and the path
+config_file="./Digest_verifier/repo_url.txt"
 
-# Check if the repository URL file exists
-if [ ! -f "$REPO_URL_FILE" ]; then
-  echo "Error: Repository URL file not found: $REPO_URL_FILE"
+# Check if the configuration file exists
+if [ ! -f "$config_file" ]; then
+  echo "Error: Configuration file not found: $config_file"
   exit 1
 fi
 
-# Read the repository URL from the file
-REPO_URL=$(cat "$REPO_URL_FILE")
+# Read the repository URL and path from the file
+read -r repo_url file_path < <(cat "$config_file" | awk -F';' '{print $1, $2}')
 
-# Check if a branch name was provided as an argument
-if [ $# -eq 1 ]; then
-  BRANCH_NAME="$1"
-  if [ "$BRANCH_NAME" = "latest" ]; then
-    # Fetch the latest branch name matching 'rhoai' from the remote repository
-    BRANCH_NAME=$(git ls-remote --heads $REPO_URL | grep 'rhoai' | awk -F'/' '{print $NF}' | sort -V | tail -1)
-    if [ -z "$BRANCH_NAME" ]; then
-      echo "Error: Unable to determine the latest branch name."
-      exit 1
-    fi
-  fi
-else
-  # Default to the latest 'rhoai' branch if no argument is provided
-  BRANCH_NAME=$(git ls-remote --heads $REPO_URL | grep 'rhoai' | awk -F'/' '{print $NF}' | sort -V | tail -1)
-  if [ -z "$BRANCH_NAME" ]; then
-    echo "Error: Unable to determine the latest branch name."
-    exit 1
-  fi
+# Validate that both the URL and the path have been read
+if [[ -z "$repo_url" || -z "$file_path" ]]; then
+  echo "Error: Repository URL or file path is missing in $config_file"
+  exit 1
 fi
 
-echo "Attempting to clone the branch '$BRANCH_NAME' from '$REPO_URL' into 'kserve' directory..."
+# Function to fetch the latest branch with a specific pattern, or default to master if not found
+fetch_latest_branch() {
+  local repo_url="$1"
+  local pattern="${2:-rhoai}"
+  local branch=$(git ls-remote --heads "$repo_url" | grep "$pattern" | awk -F'/' '{print $NF}' | sort -V | tail -1)
+  if [ -z "$branch" ]; then
+    echo "No branch matching the pattern '$pattern' found. Defaulting to 'master'."
+    branch="master"
+  fi
+  echo "$branch"
+}
+
+# Determine the branch name based on input argument
+if [ $# -eq 1 ]; then
+  if [ "$1" = "latest" ]; then
+    branch_name=$(fetch_latest_branch "$repo_url")
+  else
+    branch_name="$1"
+  fi
+else
+  branch_name=$(fetch_latest_branch "$repo_url")
+fi
+
+echo "Attempting to clone the branch '$branch_name' from '$repo_url' into 'kserve' directory..."
 
 # Clone the specified branch of the repository
-git clone --depth 1 -b "$BRANCH_NAME" "$REPO_URL" "kserve"
+git clone --depth 1 -b "$branch_name" "$repo_url" "kserve"
 if [ $? -ne 0 ]; then
-  echo "Error: Failed to clone branch '$BRANCH_NAME' from '$REPO_URL'"
+  echo "Error: Failed to clone branch '$branch_name' from '$repo_url'"
   exit 1
 else
-  echo "Successfully cloned the branch '$BRANCH_NAME'."
+  echo "Successfully cloned the branch '$branch_name'."
 fi
 
-
-# Define the path to the file you want to check in the cloned directory
-FILE_PATH="kserve/config/overlays/odh/params.env"
+# Define the full path to the file you want to check in the cloned directory
+full_path="kserve/$file_path"
 
 # Initialize a variable to keep track of SHA mismatches
 sha_mismatch_found=0
 
 # Function to check SHAs and print results
-extract_names_with_sbom_extension() {
+extract_names_with_att_extension() {
  local tag="$1"
- local hash="$2"
+ local repo_hash="$2"
 
- if [ -z "$hash" ]; then
+  if [ -z "$hash" ]; then
     echo "Error: The $name image is referenced using floating tags. Exiting..."
     exit 1
  fi
 
  json_response=$(curl -s https://quay.io/api/v1/repository/modh/$tag/tag/ | jq -r '.tags | .[:3] | map(select(.name | endswith(".att"))) | .[].name')
- local names_part=$(echo "$json_response" | sed 's/^sha256-\(.*\)\.att$/\1/')
- echo "$names_part"
+ local quay_hash=$(echo "$json_response" | sed 's/^sha256-\(.*\)\.att$/\1/')
 
- if [ "$hash" = "$names_part" ]; then
-     # Print in green if equal
-     echo "$tag"
-     echo -e "\e[32m$hash\e[0m matches \e[32m$names_part\e[0m"
+ if [ "$repo_hash" = "$quay_hash" ]; then
+     echo -e "\e[32mRepository SHA ($repo_hash) matches Quay SHA ($quay_hash) for tag: $tag\e[0m"
  else
-     # Print in red if not equal
-     echo "$tag"
-     echo -e "\e[31m$hash\e[0m does not match \e[31m$names_part\e[0m"
+     echo -e "\e[31mRepository SHA ($repo_hash) does NOT match Quay SHA ($quay_hash) for tag: $tag\e[0m"
      sha_mismatch_found=1
  fi
 }
 
 # Main logic for processing the file and SHAs
 main() {
- if [ -f "$FILE_PATH" ]; then
-     echo "File found: $FILE_PATH"
-     local input=$(<"$FILE_PATH")
+ if [ -f "$full_path" ]; then
+     echo "File found: $full_path"
+     local input=$(<"$full_path")
 
      while IFS= read -r line; do
          local name=$(echo "$line" | cut -d'=' -f1)
          local hash=$(echo "$line" | awk -F 'sha256:' '{print $2}')
-         extract_names_with_sbom_extension $name $hash
+         extract_names_with_att_extension "$name" "$hash"
      done <<< "$input"
  else
-     echo "File not found: $FILE_PATH"
+     echo "File not found: $full_path"
  fi
 
  # Check if any SHA mismatches were found
