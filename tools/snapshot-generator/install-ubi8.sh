8ff83a56549646b877f3db0042162962e079e70b
Christopher Kodama <ckodama@redhat.com>
2025-01-27 15:48:29 -0500

    add ubi8 install script for automation purposes

---
+++ tools/snapshot-generator/install-ubi8.sh
@@ -0,0 +1,15 @@
+cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/repodata/repomd.xml.key
+EOF
+
+microdnf install python3.12 kubectl jq skopeo git 
+
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
