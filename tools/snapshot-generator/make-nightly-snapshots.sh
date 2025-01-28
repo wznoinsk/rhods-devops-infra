422b23d560d2c5989c2073dba360ae077fa64d29
Christopher Kodama <ckodama@redhat.com>
2025-01-28 10:37:28 -0500

    add option for output dir

--- tools/snapshot-generator/make-nightly-snapshots.sh
+++ tools/snapshot-generator/make-nightly-snapshots.sh
@@ -23,6 +23,12 @@ fi
 
 # example: 2.17.0
 rhoai_version=$1
+shift
+
+output_file=
+if [ "$1" = "-o" ]; then
+ output_file=$2 
+fi
 
 if [ -z "$rhoai_version" ]; then
   echo "Please indicate which RHOAI version for the snapshot"
@@ -67,9 +73,9 @@ workspace=$(mktemp -d)
 echo "workspace=${workspace}"
 
 epoch=$(date +%s)
-release_artifacts_dir=nightly-snapshots
-snapshot_components_dir=${release_artifacts_dir}/snapshot-components
-snapshot_fbc_dir=${release_artifacts_dir}/snapshot-fbc
+output_dir=nightly-snapshots
+snapshot_components_dir=${output_dir}/snapshot-components
+snapshot_fbc_dir=${output_dir}/snapshot-fbc
 
 mkdir -p ${snapshot_components_dir}
 mkdir -p ${snapshot_fbc_dir}
@@ -102,6 +108,7 @@ RHOAI_KONFLUX_COMPONENTS_DETAILS_FILE_PATH=${workspace}/konflux_components.txt
 kubectl get components -o=jsonpath="{range .items[?(@.spec.application=='${component_application}')]}{@.metadata.name}{'\t'}{@.spec.containerImage}{'\n'}{end}" > ${RHOAI_KONFLUX_COMPONENTS_DETAILS_FILE_PATH}
 
 
-RHOAI_QUAY_API_TOKEN=${RHOAI_QUAY_API_TOKEN} python release_processor.py --operation generate-snapshots --catalog-yaml-path ${CATALOG_YAML_PATH} --konflux-components-details-file-path ${RHOAI_KONFLUX_COMPONENTS_DETAILS_FILE_PATH} --rhoai-version ${rhoai_version} --rhoai-application ${component_application} --epoch ${epoch} --output-dir ${release_artifacts_dir} --template-dir ${template_dir} --rbc-release-commit ${RBC_RELEASE_BRANCH_COMMIT}
+RHOAI_QUAY_API_TOKEN=${RHOAI_QUAY_API_TOKEN} python release_processor.py --operation generate-snapshots --catalog-yaml-path ${CATALOG_YAML_PATH} --konflux-components-details-file-path ${RHOAI_KONFLUX_COMPONENTS_DETAILS_FILE_PATH} --rhoai-version ${rhoai_version} --rhoai-application ${component_application} --epoch ${epoch} --output-dir ${output_dir} --template-dir ${template_dir} --rbc-release-commit ${RBC_RELEASE_BRANCH_COMMIT}
 
+cp ${snapshot_components_dir}/snapshot-components-stage-${component_application}-$epoch.yaml $output_file
 
