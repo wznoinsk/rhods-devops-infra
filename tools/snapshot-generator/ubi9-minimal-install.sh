13d646b57ae4fa1312315c6ebf6c37c79b4af6ae
Christopher Kodama <ckodama@redhat.com>
2025-01-29 15:46:55 -0500

    update to python3 instead of python3.12 for ubi9

--- tools/snapshot-generator/ubi9-minimal-install.sh
+++ tools/snapshot-generator/ubi9-minimal-install.sh
@@ -6,7 +6,7 @@ enabled=1
 gpgcheck=1
 gpgkey=https://pkgs.k8s.io/core:/stable:/v1.32/rpm/repodata/repomd.xml.key
 EOF
-microdnf install -y git shadow-utils findutils python3.12 kubectl jq skopeo
+microdnf install -y git shadow-utils findutils python3 kubectl jq skopeo
 
 python3 -m venv venv
 source venv/bin/activate
